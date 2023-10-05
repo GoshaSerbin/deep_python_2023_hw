79137064387fbcf271c0846222e006f55c00d3ae
GoshaSerbin <gosha_serbin00@mail.ru>
2023-10-05 16:45:06 +0300

    Added hw3

---
+++ 03/CustomList.py
@@ -0,0 +1,48 @@
+class CustomList(list):
+    def __str__(self):
+        return super().__str__() + f", sum = {sum(self)}"
+
+    def __eq__(self, other):
+        return sum(self) == sum(other)
+
+    def __ne__(self, other):
+        return sum(self) != sum(other)
+
+    def __ge__(self, other):
+        return sum(self) >= sum(other)
+
+    def __le__(self, other):
+        return sum(self) <= sum(other)
+
+    def __gt__(self, other):
+        return sum(self) > sum(other)
+
+    def __lt__(self, other):
+        return sum(self) < sum(other)
+
+    def __operate(self, other, operation):
+        result = CustomList([0 for _ in range(max(len(self), len(other)))])
+        i = 0
+        while i < min(len(self), len(other)):
+            result[i] = getattr(self[i], operation)(other[i])
+            i += 1
+        while i < len(self):
+            result[i] = getattr(self[i], operation)(0)
+            i += 1
+        while i < len(other):
+            result[i] = getattr(0, operation)(other[i])
+            i += 1
+
+        return result
+
+    def __add__(self, other):
+        return self.__operate(other, "__add__")
+
+    def __sub__(self, other):
+        return self.__operate(other, "__sub__")
+
+    def __radd__(self, other):
+        return self.__operate(other, "__add__")
+
+    def __rsub__(self, other):
+        return self.__operate(other, "__sub__")
---
+++ 03/test_CustomList.py
@@ -0,0 +1,104 @@
+import unittest
+
+from CustomList import CustomList
+
+
+class TestCustomList(unittest.TestCase):
+    def test_str_for_empty_list(self):
+        actual = str(CustomList([]))
+        expected = "[], sum = 0"
+        self.assertEqual(actual, expected)
+
+    def test_str_for_basic_list(self):
+        actual = str(CustomList([1, 2, 3, 4, 5]))
+        expected = "[1, 2, 3, 4, 5], sum = 15"
+        self.assertEqual(actual, expected)
+
+    def test_eq_empty_lists(self):
+        self.assertTrue(CustomList([]) == CustomList([]))
+
+    def test_eq_lists(self):
+        self.assertTrue(CustomList([1, 2, 3, 4, 5]) == CustomList([15]))
+        self.assertFalse(CustomList([1, 2]) == CustomList([1]))
+
+    def test_un_lists(self):
+        self.assertTrue(CustomList([1, 2, 3]) != CustomList([1, 2, 3, 4, 5]))
+        self.assertFalse(CustomList([0, 5]) != CustomList([5]))
+
+    def test_ge_lists(self):
+        self.assertTrue(CustomList([1, 2, 3, 4]) >= CustomList([1, 2]))
+        self.assertTrue(CustomList([1, 2]) >= CustomList([1, 2]))
+        self.assertFalse(CustomList([1, 2]) >= CustomList([1, 2, 3]))
+
+    def test_gt_lists(self):
+        self.assertTrue(CustomList([1, 2, 3, 4]) > CustomList([1, 2]))
+        self.assertFalse(CustomList([1, 2]) > CustomList([1, 2]))
+        self.assertFalse(CustomList([1, 2]) >= CustomList([1, 2, 3]))
+
+    def test_le_lists(self):
+        self.assertFalse(CustomList([1, 2, 3, 4]) <= CustomList([1, 2]))
+        self.assertTrue(CustomList([1, 2]) <= CustomList([1, 2]))
+        self.assertTrue(CustomList([1, 2]) <= CustomList([1, 2, 3]))
+
+    def test_lt_lists(self):
+        self.assertFalse(CustomList([1, 2, 3, 4]) < CustomList([1, 2]))
+        self.assertFalse(CustomList([1, 2]) < CustomList([1, 2]))
+        self.assertTrue(CustomList([1, 2]) < CustomList([1, 2, 3]))
+
+    def test_add_unequal_custom_lists(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = CustomList([1, 2, 3, 4, 5, 6])
+        lists_sum = left_list + right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [1, 2, 3, 4, 5, 6])
+        self.assertListEqual(list(lists_sum), [2, 4, 6, 8, 5, 6])
+
+    def test_add_equal_custom_lists(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = CustomList([0, 1, 0, 1])
+        lists_sum = left_list + right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [0, 1, 0, 1])
+        self.assertListEqual(list(lists_sum), [1, 3, 3, 5])
+
+    def test_sub_unequal_custom_lists(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = CustomList([1, 2, 3, 4, 5, 6])
+        lists_sub = left_list - right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [1, 2, 3, 4, 5, 6])
+        self.assertListEqual(list(lists_sub), [0, 0, 0, 0, -5, -6])
+
+    def test_sub_equal_custom_lists(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = CustomList([0, 1, 0, 1])
+        lists_sub = left_list - right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [0, 1, 0, 1])
+        self.assertListEqual(list(lists_sub), [1, 1, 3, 3])
+
+    def test_with_left_list_greater_than_right_list(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = CustomList([0, 1])
+        lists_sub = left_list - right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [0, 1])
+        self.assertListEqual(list(lists_sub), [1, 1, 3, 4])
+
+    def test_custom_list_add_base_list(self):
+        left_list = CustomList([1, 2, 3, 4])
+        right_list = [2, 5]
+        lists_sum = left_list + right_list
+        self.assertListEqual(list(left_list), [1, 2, 3, 4])
+        self.assertListEqual(list(right_list), [2, 5])
+        self.assertListEqual(list(lists_sum), [3, 7, 3, 4])
+        self.assertIsInstance(lists_sum, CustomList)
+
+    def test_base_list_add_custom_list(self):
+        left_list = [2, 5]
+        right_list = CustomList([1, 2, 3, 4])
+        lists_sum = left_list + right_list
+        self.assertListEqual(list(left_list), [2, 5])
+        self.assertListEqual(list(right_list), [1, 2, 3, 4])
+        self.assertListEqual(list(lists_sum), [3, 7, 3, 4])
+        self.assertIsInstance(lists_sum, CustomList)
