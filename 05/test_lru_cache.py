bb02418ea7f1b647ad040ffa5c4878a4515491b7
GoshaSerbin <gosha_serbin00@mail.ru>
2023-11-19 14:58:48 +0300

    added tests

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -57,3 +57,43 @@ class TestLRUCache(unittest.TestCase):
         self.assertIs(cache.get("k2"), None)
         self.assertEqual(cache.get("k1"), "new_val1")
         self.assertEqual(cache.get("k3"), "val3")
+
+    def test_case_like_in_task(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertEqual(cache.get("k3"), None)
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
+
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k1"), "val1")
+
+    def test_with_cache_size_eq_1(self):
+        cache = LRUCache(1)
+
+        cache.set("k1", "val1")
+        self.assertEqual(cache.get("k1"), "val1")
+        cache.set("k2", "val2")
+        self.assertEqual(cache.get("k1"), None)
+        self.assertEqual(cache.get("k2"), "val2")
+        cache.set("k3", "val3")
+        self.assertEqual(cache.get("k1"), None)
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k3"), "val3")
+
+    def test_changing_existing_key_and_consequences(self):
+        cache = LRUCache(2)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k1", "new_val1")
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k1"), "new_val1")
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k3"), "val3")
